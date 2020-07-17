25601626731f861035954ba180c09ffdf2006dc3
abhishek1501kholiya <abhishekkoliya15012007@gmail.com>
2020-07-17 15:51:23 +0530

    hello

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, jumper1 , jumper2 , jumper3 , jumper4;
+
+var track, car1_img, car2_img, car3_img, car4_img;
+
+function preload(){
+  track = loadImage("../images/track.jpg");
+  car1_img = loadImage("../images/car1.png");
+  car2_img = loadImage("../images/car2.png");
+  car3_img = loadImage("../images/car3.png");
+  car4_img = loadImage("../images/car4.png");
+  ground = loadImage("../images/ground.png");
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    //line(200,200,10,4200)
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+  }
+}
